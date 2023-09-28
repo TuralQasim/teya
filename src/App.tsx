ab4f23396c72606fde470badb8b2ce10dcec4fa4
TuqalQasim <qasimovtural572@gmail.com>
2023-09-28 20:19:56 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Teya from "./Teya";
+
+function App() {
+  return (
+    <>
+      <Teya />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Teya.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Teya() {
+  return (
+    <>
+      <div className="container">teya</div>
+    </>
+  );
+}
+
+export default Teya;
